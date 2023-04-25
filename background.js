39980e789e1a8ca3385ed52c23384c722197c68e
Patrick <Conrad@pcmacdev.myfiosgateway.com>
2023-04-25 06:00:35 -0700

    first

--- background.js
+++ background.js
@@ -10,6 +10,17 @@ async function execScript() {
 }
 
 async function getTabId() {
+  const allTabs = await chrome.tabs.query({});
+  console.log(typeof(allTabs))
+  const tabsArray = Object.values(allTabs);
+  console.log(tabsArray);
+  console.log(typeof(tabsArray))
+  console.log(tabsArray[0])
+
+  const workTab = tabsArray.find(t=>{
+    return t.url.startsWith('https://www.a4');
+  })
+  console.log(workTab)
   const tabs = await chrome.tabs.query({active: true, currentWindow: true});
   return (tabs.length > 0) ? tabs[0].id : null;
 }
--- execute.js
+++ execute.js
@@ -1,4 +1,38 @@
-// this code will be executed when the extension's button is clicked
+// const mousePosition = [0, 0, 0, 0];
+
+// const setMousePosition = e => {
+//   // console.log(e);
+// }
+
+const getElementRect = (element) => {
+  return element.getBoundingClientRect()
+}
+
+const elementsByPosition = []
+const getChildren = (element, childPos, colorIndex) => {
+  if(!element.childNodes || element.childNodes.length == undefined || element.childNodes.length == null || element.childNodes.length <1){
+    elementsByPosition.push([12, childPos, 'none'])
+    return;
+  }
+  const rect = getElementRect(element);
+  console.log(rect);
+
+  elementsByPosition.push([colorIndex, childPos, rect])
+  const childArray = Object.values(element.childNodes);
+  console.log(childArray)
+  for(let i = 0; i <= childArray.length; i++){
+    const next = element.querySelector(`:nth-child(${i+1})`)
+    console.log(next);
+    getChildren(next, [...childPos, i], colorIndex+1)
+  }
+  return;
+}
+
+
 (function() {
-  console.log('execute.js executed');
-})();
+  const body = document.querySelector('body');
+  // body.addEventListener('onMouseMove', (e)=> setMousePosition(e));
+  // console.log(body);
+  getChildren(body, [0], 0)
+})()
+
